a5cb01ff9e241bd18fc8184535415abcdca1d257
Dawid Szymaniak <69818774+dawid-sz@users.noreply.github.com>
2022-03-18 22:25:23 +0100

    functional updates
    Some functional and visual updates. Save Tab button added but without a function right now. Visually added some hover effects. Code got refactor, used parameters inside a function and arguments to invoke it. Passing few new comments in.

--- index.js
+++ index.js
@@ -4,35 +4,44 @@ const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.querySelector("#save-btn");
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 };
+
+const tabs = [
+    {url: "link to the profile"}
+];
+
+tabBtn.addEventListener("click", function() {
+    console.log(tabs[0].url)
+});
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
-})
+    render(myLeads);
+});
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) ) //Turning array into a string to safe input into the local storage
-    renderLeads();
+    render(myLeads);
 });
 
 // creating a function to wrap the whole code and put it into inputBtn function
-function renderLeads() {
+function render(leads) { //creating a parameter leads for the function
     let listItems = "" // <- creating a "let" variable (because it will be reassigned), listItems, to hold all the HTML for the list items
                        // assigning it to an empty string to begin with
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         //adding items to the listItems to increase performance of the code
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
